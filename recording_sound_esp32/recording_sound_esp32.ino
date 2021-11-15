02fe24984772aa115c93af43af968574f13fa4cd
kngjnssy <kingajanossy@gmail.com>
2021-11-15 19:04:09 +0100

    removing unnecessary functionality

--- recording_sound_esp32/recording_sound_esp32.ino
+++ recording_sound_esp32/recording_sound_esp32.ino
@@ -41,44 +41,8 @@ File root;
 bool opened = false;
 
 // save recording
-char filename[] = "new.wav";
+char filename[] = "new1.wav";
 const int headerSize = 44;
-
-String serverIndex = "<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>"
-"<form method='POST' action='#' enctype='multipart/form-data' id='upload_form'>"
-    "<input type='file' name='update'>"
-    "<input type='submit' value='Upload'>"
-"</form>"
-"<div id='prg'>progress: 0%</div>"
-"<script>"
-"$('form').submit(function(e){"
-    "e.preventDefault();"
-      "var form = $('#upload_form')[0];"
-      "var data = new FormData(form);"
-      " $.ajax({"
-            "url: '/update',"
-            "type: 'POST',"               
-            "data: data,"
-            "contentType: false,"                  
-            "processData:false,"  
-            "xhr: function() {"
-                "var xhr = new window.XMLHttpRequest();"
-                "xhr.upload.addEventListener('progress', function(evt) {"
-                    "if (evt.lengthComputable) {"
-                        "var per = evt.loaded / evt.total;"
-                        "$('#prg').html('progress: ' + Math.round(per*100) + '%');"
-                    "}"
-               "}, false);"
-               "return xhr;"
-            "},"                                
-            "success:function(d, s) {"    
-                "console.log('success!')"
-           "},"
-            "error: function (a, b, c) {"
-            "}"
-          "});"
-"});"
-"</script>";
 
 String printDirectory(File dir, int numTabs) {
   String response = "";
@@ -102,7 +66,9 @@ String printDirectory(File dir, int numTabs) {
     }
     entry.close();
   }
-  return String("List files:</br>") + response + String("</br></br> Upload file:") + serverIndex;
+  //  return String("List files:</br>") + response + String("</br></br> Upload file:") + serverIndex;
+  //  no need for upload, jsut list files on SD card:
+  return String("List files:</br>") + response;
 }
 
 bool loadFromSDCARD(String path) {
@@ -256,13 +222,6 @@ void sdInit() {
     return;
   }
   Serial.println("initialization done.");
-
-//  root = SD.open("/");
-//  if (root) {
-//    root.close();
-//  } else {
-//    Serial.println("error opening root?");
-//  }
 
   SD.remove(filename);
   file = SD.open(filename, FILE_WRITE);
