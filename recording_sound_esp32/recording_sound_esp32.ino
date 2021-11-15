efe1e4a4df7fa81e5334d678c3b7d5ded930af52
kngjnssy <kingajanossy@gmail.com>
2021-11-15 18:54:58 +0100

    added web server, recorded file can be downloaded

--- recording_sound_esp32/recording_sound_esp32.ino
+++ recording_sound_esp32/recording_sound_esp32.ino
@@ -1,3 +1,4 @@
+
 // Connect SD Card Module pins as following:
 // CS to 5
 // SCK to 18
@@ -7,6 +8,11 @@
 // GND to GND
 
 #include <driver/i2s.h>
+#include <WiFiClient.h>
+#include <ESP32WebServer.h>
+#include <WiFi.h>
+#include <ESPmDNS.h>
+#include <SPI.h>
 #include <mySD.h>
 
 // connect Mic pins as following:
@@ -25,15 +31,152 @@
 #define I2S_CHANNEL_NUM   (1)
 #define FLASH_RECORD_SIZE (I2S_CHANNEL_NUM * I2S_SAMPLE_RATE * I2S_SAMPLE_BITS / 8 * RECORD_TIME)
 
+const char* ssid = "K";
+const char* password = "szilicica";
+
 File file;
+
+ESP32WebServer server(80);
 File root;
+bool opened = false;
 
 // save recording
 char filename[] = "new.wav";
 const int headerSize = 44;
 
+String serverIndex = "<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>"
+"<form method='POST' action='#' enctype='multipart/form-data' id='upload_form'>"
+    "<input type='file' name='update'>"
+    "<input type='submit' value='Upload'>"
+"</form>"
+"<div id='prg'>progress: 0%</div>"
+"<script>"
+"$('form').submit(function(e){"
+    "e.preventDefault();"
+      "var form = $('#upload_form')[0];"
+      "var data = new FormData(form);"
+      " $.ajax({"
+            "url: '/update',"
+            "type: 'POST',"               
+            "data: data,"
+            "contentType: false,"                  
+            "processData:false,"  
+            "xhr: function() {"
+                "var xhr = new window.XMLHttpRequest();"
+                "xhr.upload.addEventListener('progress', function(evt) {"
+                    "if (evt.lengthComputable) {"
+                        "var per = evt.loaded / evt.total;"
+                        "$('#prg').html('progress: ' + Math.round(per*100) + '%');"
+                    "}"
+               "}, false);"
+               "return xhr;"
+            "},"                                
+            "success:function(d, s) {"    
+                "console.log('success!')"
+           "},"
+            "error: function (a, b, c) {"
+            "}"
+          "});"
+"});"
+"</script>";
+
+String printDirectory(File dir, int numTabs) {
+  String response = "";
+  dir.rewindDirectory();
+
+  while (true) {
+    File entry =  dir.openNextFile();
+    if (! entry) {
+      // no more files
+      //Serial.println("**nomorefiles**");
+      break;
+    }
+    for (uint8_t i = 0; i < numTabs; i++) {
+      Serial.print('\t');   // we'll have a nice indentation
+    }
+    // Recurse for directories, otherwise print the file size
+    if (entry.isDirectory()) {
+      printDirectory(entry, numTabs + 1);
+    } else {
+      response += String("<a href='") + String(entry.name()) + String("'>") + String(entry.name()) + String("</a>") + String("</br>");
+    }
+    entry.close();
+  }
+  return String("List files:</br>") + response + String("</br></br> Upload file:") + serverIndex;
+}
+
+bool loadFromSDCARD(String path) {
+  path.toLowerCase();
+  String dataType = "text/plain";
+  if (path.endsWith("/")) path += "index.htm";
+
+  if (path.endsWith(".src")) path = path.substring(0, path.lastIndexOf("."));
+  else if (path.endsWith(".jpg")) dataType = "image/jpeg";
+  else if (path.endsWith(".txt")) dataType = "text/plain";
+  else if (path.endsWith(".zip")) dataType = "application/zip";
+  Serial.println(dataType);
+  File dataFile = SD.open(path.c_str());
+
+  if (!dataFile)
+    return false;
+
+  if (server.streamFile(dataFile, dataType) != dataFile.size()) {
+    Serial.println("Sent less data than expected!");
+  }
+
+  dataFile.close();
+  return true;
+}
+
+void handleNotFound() {
+  if (loadFromSDCARD(server.uri())) return;
+  String message = "SDCARD Not Detected\n\n";
+  message += "URI: ";
+  message += server.uri();
+  message += "\nMethod: ";
+  message += (server.method() == HTTP_GET) ? "GET" : "POST";
+  message += "\nArguments: ";
+  message += server.args();
+  message += "\n";
+  for (uint8_t i = 0; i < server.args(); i++) {
+    message += " NAME:" + server.argName(i) + "\n VALUE:" + server.arg(i) + "\n";
+  }
+  server.send(404, "text/plain", message);
+  Serial.println(message);
+}
+
+void handleRoot() {
+  root = SD.open("/");
+  String res = printDirectory(root, 0);
+  server.send(200, "text/html", res);
+}
+
+void wifiInit() {
+  WiFi.begin(ssid, password);
+  Serial.println("");
+
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  //use IP or iotsharing.local to access webserver
+  if (MDNS.begin("iotsharing")) {
+    Serial.println("MDNS responder started");
+  }
+}
+
 void setup() {
   Serial.begin(115200);
+
+  wifiInit();
+
   try {
     sdInit();
   }
@@ -68,11 +211,41 @@ void setup() {
   catch (...) {
     Serial.println("RECORDING FAILED");
   }
+
+  //handle uri
+  server.on("/", handleRoot);
+  server.onNotFound(handleNotFound);
+
+  /*handling uploading file */
+  server.on("/update", HTTP_POST, []() {
+    server.sendHeader("Connection", "close");
+  }, []() {
+    HTTPUpload& upload = server.upload();
+    if (opened == false) {
+      opened = true;
+      root = SD.open((String("/") + upload.filename).c_str(), FILE_WRITE);
+      if (!root) {
+        Serial.println("- failed to open file for writing");
+        return;
+      }
+    }
+    if (upload.status == UPLOAD_FILE_WRITE) {
+      if (root.write(upload.buf, upload.currentSize) != upload.currentSize) {
+        Serial.println("- failed to write");
+        return;
+      }
+    } else if (upload.status == UPLOAD_FILE_END) {
+      root.close();
+      Serial.println("UPLOAD_FILE_END");
+      opened = false;
+    }
+  });
+  server.begin();
+  Serial.println("HTTP server started");
 }
 
 void loop() {
-  // The ESP32 will be in deep sleep
-  // it never reaches the loop()
+  server.handleClient();
 }
 
 void sdInit() {
@@ -84,12 +257,12 @@ void sdInit() {
   }
   Serial.println("initialization done.");
 
-  root = SD.open("/");
-  if (root) {
-    root.close();
-  } else {
-    Serial.println("error opening root?");
-  }
+//  root = SD.open("/");
+//  if (root) {
+//    root.close();
+//  } else {
+//    Serial.println("error opening root?");
+//  }
 
   SD.remove(filename);
   file = SD.open(filename, FILE_WRITE);
